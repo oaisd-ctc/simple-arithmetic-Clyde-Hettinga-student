34bbbefead06eb3538338df930acfa429c0de4e7
Clyde-Hettinga-student <nahe406@student.oaisd.org>
2024-10-11 17:04:44 +0000

    Made most of the input fields and one catch for bad inputs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class BasicMath
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Select an operation:\nAddition (add)\nSubtraction (sub)\nMultiplication (mult)\nDivision (div)\nModulus (abs)");
+        string operation = Console.ReadLine();
+
+        while(!operation.ToLower().Equals("add") && !operation.ToLower().Equals("sub") && !operation.ToLower().Equals("mult") && !operation.ToLower().Equals("div")
+              && !operation.ToLower().Equals("abs"))
+        {
+            Console.WriteLine("\nEnter \"add\", \"sub\", \"mult\", \"div\", or \"abs\"");
+            operation = Console.ReadLine();
+        }
+
+        Console.Write("\nEnter first number: ");
+        string numStr1 = Console.ReadLine();
+
+        Console.Write("\nEnter second number: ");
+        string numStr2 = Console.ReadLine();
+
+        if(operation.ToLower().Equals("add"))
+        {}
+        else if(operation.ToLower().Equals("sub"))
+        {}
+        else if(operation.ToLower().Equals("mult"))
+        {}
+        else if(operation.ToLower().Equals("div"))
+        {}
+        else
+        {}
+    }
+}
